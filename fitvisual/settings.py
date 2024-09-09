127c63944f82e6d2aba7bc2f732f8eeefbb9a9d1
HarmanHakim <harmanhakim99@gmail.com>
2024-09-09 22:49:43 +0700

    second commit

--- fitvisual/settings.py
+++ fitvisual/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a8y6-xwtxk=vxx1yciz6fk_s%knu(2zeckbd+ali--e13f52h&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "harman-hakim-fitvisual.pbp.cs.ui.ac.id"]
 
 
 # Application definition
