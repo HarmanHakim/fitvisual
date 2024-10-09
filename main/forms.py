fedaf7ab828ea0aa6c3b07fed1400f9f4785a126
HarmanHakim <harmanhakim99@gmail.com>
2024-10-09 11:19:14 +0700

    Tugas 6

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
 from main.models import TokoEntry
+from django.utils.html import strip_tags
 
 class TokoEntryForm(ModelForm):
     class Meta:
         model = TokoEntry
         fields = ["nama", "harga", "description", "image_url"]
+    
+    def clean_product(self):
+        product = self.cleaned_data["product"]
+        return strip_tags(product)
--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from main.views import show_main, create_toko_entry, edit_toko, delete_toko, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from main.views import show_main, create_toko_entry, edit_toko, delete_toko, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, add_product_entry_ajax
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-toko-entry', create_toko_entry, name='create_toko_entry'),
-    path('edit-toko/<uuid:id>', edit_toko, name='edit_toko'),
-    path('delete-toko/<uuid:id>', delete_toko, name='delete_toko'),
+    path('edit_toko/<uuid:id>', edit_toko, name='edit_toko'),
+    path('delete_toko/<uuid:id>', delete_toko, name='delete_toko'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
@@ -15,5 +15,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('create-ajax', add_product_entry_ajax, name='create-ajax'),
 ]
 
